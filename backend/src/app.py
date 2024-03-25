9796880026708dc323bfdbcff2aa315a6f126eda
NouranElmenshawy <147437528+NouranElmenshawy@users.noreply.github.com>
2024-03-25 13:37:15 +0100

    Update app.py

--- backend/src/app.py
+++ backend/src/app.py
@@ -99,4 +99,5 @@ def handle_get_active_rooms():
     emit('active_rooms', {'rooms': active_rooms})
     
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    socketio.run(app, host='0.0.0.0', port=port)
